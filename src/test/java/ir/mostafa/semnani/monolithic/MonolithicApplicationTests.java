3cc527e3232127b71d9886d25492b7737ae8d62c
mostafa-semnani-74 <mostafa.semnani.74@gmail.com>
2023-05-11 12:34:21 +0330

    init spring boot

---
+++ src/test/java/ir/mostafa/semnani/monolithic/MonolithicApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mostafa.semnani.monolithic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
